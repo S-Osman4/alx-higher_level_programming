fbd91357c6e6690db7210d4817483fe8a16d74d8
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-06-20 13:39:27 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print((my_list[i]), end="")
             return_element = return_element + 1
         except IndexError:
-            break
-        print("")
+            None
+        print()
         return (return_element)
