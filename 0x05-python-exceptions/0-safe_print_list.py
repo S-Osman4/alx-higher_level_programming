bae41d96c325a6c9dda8d595dad28f5b4a1b93ef
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-06-20 13:37:36 +0300

    missing bracket

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     return_element = 0
     for i in range(x):
         try:
-            print(my_list[i]), end="")
+            print((my_list[i]), end="")
             return_element = return_element + 1
         except IndexError:
             break
