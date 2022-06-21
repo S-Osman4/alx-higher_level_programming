e42675531683ac07c9641b60040ddfba113babf3
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-06-21 22:37:07 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-""" creates class Square """
+""" Module """
 
 
 class Square:
-    """ Square class"""
+    """Defines the square"""
     def __init__(self, size=0):
+        """Init the square class
+        Args:
+            size (int): size of square `size`.
+        Raises:
+            TypeError: The ``size`` size must be an integer.
+            ValueError: ``size`` must be >= 0.        
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
