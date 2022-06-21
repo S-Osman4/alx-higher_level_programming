840f5b9ab1d3f2e2bdc787a9ca07c4de136846c8
Shamso Osman <osmanshamso004@gmail.com>
2022-06-21 04:08:39 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module """
+
+
+
+class Square:
+    """Defines the square"""
+    def __init__(self, size=0):
+    """Constructor
+    Args:
+        size (int): size of square `size`.
+    size (int): size of square `size`.
+        TypeError: The ``size`` size must be an integer.
+        ValueError: ``size`` must be >= 0.
+    """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
