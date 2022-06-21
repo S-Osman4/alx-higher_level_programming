45b78c90395ab228463a5d92900230e8dbef2636
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-06-21 14:09:18 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,9 +9,10 @@ class Square:
     """Constructor
     Args:
         size (int): size of square `size`.
-    size (int): size of square `size`.
+    Raises:
         TypeError: The ``size`` size must be an integer.
         ValueError: ``size`` must be >= 0.
+        
     """
         if type(size) != int:
             raise TypeError("size must be an integer")
