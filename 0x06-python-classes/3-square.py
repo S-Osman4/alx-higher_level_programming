bfcd15f17f3bb5d1f522ab8a5d443980bc78f780
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-06-21 22:42:52 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,4 +15,4 @@ class Square:
             
     def area(self):
         """ Calculate the area of a square"""        
-        return ((self.__size) ** 2)
+        return self.__size * self.__size
