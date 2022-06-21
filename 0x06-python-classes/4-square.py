2421708802aebdeb9be0e7ac26fd7208755f0bf6
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-06-21 22:53:12 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,11 +20,11 @@ class Square:
     def size(self, value):
         if type(value) != int:
             raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
+            
     def area(self):
         """ Calculate area"""
         return self.__size * self.__size
