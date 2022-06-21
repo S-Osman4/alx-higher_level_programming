a764e64244ca940b6c54bff077d5188eaae42069
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-06-21 14:11:01 +0300

    indent block

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,14 +6,14 @@
 class Square:
     """Defines the square"""
     def __init__(self, size=0):
-    """Constructor
-    Args:
-        size (int): size of square `size`.
-    Raises:
-        TypeError: The ``size`` size must be an integer.
-        ValueError: ``size`` must be >= 0.
+        """Constructor
+        Args:
+            size (int): size of square `size`.
+        Raises:
+            TypeError: The ``size`` size must be an integer.
+            ValueError: ``size`` must be >= 0.
         
-    """
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
