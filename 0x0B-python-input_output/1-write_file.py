2fba7ef5fa6cb5911af6e7d85fcc1aacac184eb3
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-07-06 01:17:27 +0300

    fix errror

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """Module for number_of_lines function."""
 
 
-def number_of_lines(filename=""):
+def write_file(filename="", text=""):
     """This function writes a string to  file overwrites if exist."""
     with open(filename, mode='w', encoding='utf-8') as f:
         len = f.write(text)
