ffebc0835836eb9c087e802708e7c48890219d32
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-07-06 01:32:45 +0300

    fix error

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Module for save_to_json function."""
-
-
+"""Module for load_from_json_file function."""
 import json
 
 
 def load_from_json_file(filename):
-    """This function loads json from file and convert it to pyobject'''
-    with open(filename, mode='r', encoding='utf-8') as f:
-        my_object = json.load(f)
-    return my_object
+    """This function creates an Object from a “JSON file”."""
+    with open(filename, "r") as f:
+        return json.load(f)
