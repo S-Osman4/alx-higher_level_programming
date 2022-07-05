aa39086b7e7e535455c97bb7b847750d453a1b1a
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-07-06 01:40:59 +0300

    appends elements in a json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to append elements in a json file."""
+import json
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+"""dump: create an object using a json and save it to a .txt file"""
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""load: read a file and convert json data to object."""
+
+try:
+    container = load_from_json_file('add_item.json')
+except:
+    container = []
+
+for i in range(1, len(sys.argv)):
+    container.append(sys.argv[i])
+save_to_json_file(container, 'add_item.json')
