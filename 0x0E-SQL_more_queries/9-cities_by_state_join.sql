566dac1604947c0ea40589aa17f0ab0cf2debb3d
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-08-10 23:06:21 +0300

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
---list all cities contained in the database hbtn_0d_usa.
+-- list all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
 FROM cities
 LEFT JOIN states
