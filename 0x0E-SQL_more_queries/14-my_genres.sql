da2ae92746b7342e389bb25b883fe657e8210648
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-08-10 23:41:07 +0300

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- lists all genres of the show Dexter.
-SELECT g.name FROM tv_genres
+SELECT tv_genres.name FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows
