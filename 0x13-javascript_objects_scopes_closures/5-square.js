952119368b989d0b13628164982be9ee8caea1ff
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-06 16:03:08 +0300

    inherit class parent

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/nodejs
+// Class Square inherits from Rectangle.
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
