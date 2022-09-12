6134f0f251d626dd88e38b4c8eb0be34fcbf3de4
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-12 15:40:19 +0300

    SQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Display all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+SQL injections
+Arguments:
+    username - username to connect the mySQL
+    mysql passwd - password to connect the mySQL
+    DB name - Name of the database
+    name - state name searched
+"""
+
+
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         host="localhost",
+                         port=3306)
+    """Connect to a MySQL server."""
+
+    cursor = db.cursor()
+    upper = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    cursor.execute(upper, (argv[4],))
+    list_of_tuples = cursor.fetchall()
+    lists = cur.fetchall()
+    for row in lists:
+        print(row)
+
+    cursor.close()
+    db.close()
