96b6ec4b829d6d09c256f75c8d3dce7a59acb028
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-23 19:46:43 +0300

    takes your GitHub credentials and uses the GitHub API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""
+    Python script that takes your Github credentials (username and password)
+    and uses the Github API to display your id
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    
+     url = "https://api.github.com/user"
+    response = requests.get(url, auth=(user, passwd)).json()
+
+    if "id" in response:
+        print(response['id'])
+    else:
+        print('None')
