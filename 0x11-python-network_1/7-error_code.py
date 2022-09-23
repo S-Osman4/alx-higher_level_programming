a466c8865254b911384dc0ce405dc39d22dd47d9
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-23 19:35:30 +0300

    script that takes in a URL, sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""
+    Python script that takes in a URL, sends a request to the URL and displays
+    the body of the response. Using package requests
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    response = requests.get(sys.argv[1])
+    if (response.status_code >= 400):
+        print('Error code:', response.status_code)
+    else:
+        print(response.text)
