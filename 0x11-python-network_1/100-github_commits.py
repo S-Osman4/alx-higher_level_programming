b9d01212b198eedd894c4b635b330f2c9ffd8096
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-23 19:55:53 +0300

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+
+"""
+    Python script that takes in a string and sends a search
+    request to the Star Wars API
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    repository_name = sys.argv[1]
+    owner = sys.argv[2]
+    headers = {
+              'Accept': 'application/vnd.github.v3+json',
+              }
+    params = {
+        'per_page': 10,
+    }
+
+    response = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
+                      owner, repository_name),
+                      headers=headers, params=params)
+    json_response = response.json()
+    for commit in json_response:
+        print(commit['sha'] + ':', commit['commit']['author']['name'])
