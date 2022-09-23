66a7f8e63a1dcc7ff1ea08ecad6faeec5190a643
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-23 19:10:24 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,9 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
-        html = resp.read()
-        print("Body resp:")
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
+        print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode(encoding="utf-8")))
