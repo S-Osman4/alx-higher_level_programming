9496a1a4b3a5688984a5934bdcf647eeaf3309a5
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-23 19:17:16 +0300

    display email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""
+    Python script that takes in a URL and an email, sends a POST request to the
+    passed URL with the email as a parameter, and displays the body of the
+    response (decoded in utf-8)
+"""
+
+if __name__ == '__main__':
+    import sys
+    from urllib import request, parse
+
+    data = parse.urlencode({'email': sys.argv[2]}).encode()
+    req = request.Request(sys.argv[1], data=data)
+    with request.urlopen(req) as res:
+        print(res.read().decode('UTF-8'))
