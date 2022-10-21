ce6c7ea8b1c8d09576036f49c97171bb3bce3180
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-21 16:16:17 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'http://swapi.co/api/films/:id' + process.argv[2];
 request(url, function (err, httpResponse, body) {
   console.log(JSON.parse(body).title || err);
 });
