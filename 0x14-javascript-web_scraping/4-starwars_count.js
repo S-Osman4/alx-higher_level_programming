349304be899d0ec8cc4cfd2de5685d1bb0d66772
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-21 16:24:56 +0300

    character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) throw error;
+  else {
+    let count = 0;
+    for (const film of JSON.parse(body).results) {
+      for (const character of film.characters) {
+        if (character.includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
